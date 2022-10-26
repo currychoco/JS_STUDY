ad15d93a6f92802ab1b4561d04ba2586f0ec1d5d
currychoco <godqhrgka0000@naver.com>
2022-10-26 09:39:54 +0900

    웹 디데이 제작, css 적용

---
+++ d-day_이혜량/d-day.js
@@ -0,0 +1,54 @@
+// 실시간으로 현재 시간을 보여주는 타이머가 있고,
+// form 안에서 디데이 연산을 할 날짜를 입력받아
+// 이벤트 명과 디데이를 표기해 주는 서비스
+
+
+const time = document.body.querySelector('.time');
+// const printdate = document.body.querySelector('.dday');
+
+const form = document.body.querySelector('form');
+form.addEventListener('submit',e=>{
+    e.preventDefault(); // 기본 동작 방지
+    const content = document.body.querySelector('.dateContent').value;
+    const uDate = document.body.querySelector('.userDate').value;
+
+    document.body.querySelector(".user-text").innerHTML = content;
+    document.body.querySelector(".dday").innerHTML = `D-${calcDday(uDate)}`;
+
+    document.body.querySelector(".before-input").style.display = "none";
+
+    document.body.querySelector(".after-header").style.display = "block";
+
+});
+
+function calcDday(uDate){
+    const userDate = new Date(uDate);
+    const curDate = new Date();
+    
+    const sub = userDate - curDate;
+    console.log(sub);
+    const dday = Math.floor(sub / (1000*60*60*24));
+    return dday;
+};
+
+function getTime(){
+    const date = new Date();
+    let hours = String(date.getHours());
+    let min = String(date.getMinutes());
+    let sec = String(date.getSeconds());
+
+    if(hours.length == 1){
+        hours = '0'.concat(hours);
+    }
+    if(min.length == 1){
+        min = '0'.concat(min);
+    }
+    if(sec.length == 1){
+        sec = '0'.concat(sec);
+    }
+
+    time.innerHTML = `${hours}:${min}:${sec}`;
+}
+
+getTime();
+setInterval(getTime, 1000);
